95bbb4db9727884a69e0748f42d2fa5c16b59db0
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-21 05:07:00 -0700

    Cleanup

--- main.go
+++ main.go
@@ -7,16 +7,7 @@ import (
 	"github.com/taylormonacelli/candleburn/myec2"
 )
 
-var (
-	tagName    string
-	region     string
-	outputFile string
-)
-
 func init() {
-	flag.StringVar(&tagName, "tag", "Name", "Specify the tag key for identifying instances")
-	flag.StringVar(&region, "region", "us-east-1", "Specify the AWS region to query")
-	flag.StringVar(&outputFile, "output", "goldpuppy.json", "Specify the output JSON file name")
 	logging.Init()
 	defer logging.Sync()
 }
